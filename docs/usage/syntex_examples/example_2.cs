34ec2f06882abbb65598f6cdbdb31babf4b62edd
Ponito <vvbdac58@gmail.com>
2026-06-17 14:38:29 +0800

    creating magic comment templating

---
+++ docs/usage/syntex_examples/example_2.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using Cysharp.Threading.Tasks;
+using Ct = System.Threading.CancellationToken;
+
+namespace Core.Apis
+{
+    public class name_controllerController
+    {
+        public static async UniTask<type_return> GetQuestions( //~rp 'type_return'
+            type_dto dto,                                      //~type_dto ? rp 'type_dto'
+            Ct ct = default) =>
+            await new RequestBuilder()
+                .AddQuery(dto)
+                .SetMethod("str_method")                       //~rp 'str_method'
+                .SetEndpoint("str_endpoint")                   //~rp 'str_endpoint'
+                .AddAuthorization()
+                .Send<type_return>(ct);                        //~rp 'type_return'
+    }
+}
